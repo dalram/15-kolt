5f5c0c8f0100d7a06fd76570434513db4836c04e
Dalius Ram <ramonas.dalius@gmail.com>
2022-05-30 19:59:01 +0300

    Kolt initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.css';
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h2>Kolt paspirtuku nuoma</h2>
+      <div className='list-box'>
+      Laba diena
+      </div>
+      {/* Modal langas turi buti atvaizduojamas paspaudus 'Redaguoti' ant paspirtuko. */}
+    </div>
+  );
+}
+
+export default App;
