214bf0dc7d7a9db96f515774bdf012c18bc9d9c8
dalram <ramonas.dalius@gmail.com>
2022-06-13 12:59:02 +0300

    update

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -14,7 +14,7 @@ function List({ scooters, setDeleteData, setModalData, sortType }) {
             {sortType === "1"
               ? scooters === null
                 ? null
-                : scooters
+                : [...scooters]
                     .sort((a, b) => a.id - b.id)
                     .map((scooter) => (
                       <Item
@@ -28,7 +28,7 @@ function List({ scooters, setDeleteData, setModalData, sortType }) {
             {sortType === "2"
               ? scooters === null
                 ? null
-                : scooters
+                : [...scooters]
                     .sort(
                       (a, b) => a.totalRideKilometres - b.totalRideKilometres
                     )
@@ -44,7 +44,7 @@ function List({ scooters, setDeleteData, setModalData, sortType }) {
             {sortType === "3"
               ? scooters === null
                 ? null
-                : scooters
+                : [...scooters]
                     .sort((a, b) => a.lastUseTime.localeCompare(b.lastUseTime))
                     .map((scooter) => (
                       <Item
